f77fddf2dac163beae83795db48ce6fd06f21056
Simone <gaminggyrrl@gmail.com>
2023-05-29 00:05:15 -0700

    added map to store grad data from sheet

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var formElement = document.getElementById('school-form');
 formElement.addEventListener('submit', handleFormSubmit);
 
 //hashmap to data:
-// let firstHash = new Map();
-const firstHash = []
+const schoolMap = new Map();
 
 function initClient() {
     // Initialize the Google Sheets API client
     gapi.client.init({
-        //i create this apikey on Google console 
-        apiKey: 'copy and paste key in here',
+        //i create this apikey on Google console
+        apiKey: 'AIzaSyCss-4b1i0PHAQAjRwKvSN9yPADeaIQS_U',
+        clientId: '964029012495-squ1jhpunge1gvnart6dqintq6qbo1ev.apps.googleusercontent.com',
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(() => {
         readData();
@@ -32,24 +32,37 @@ function readData() {
     gapi.client.sheets.spreadsheets.values.get({
         //this id is unique for every sheet (is on the link)
         //NOTE:Google sheet has to be public *** you can also change the range 
-        spreadsheetId: 'copy and paste ID',
-        range: 'Sheet1!A1:Z10',
+        spreadsheetId: '1_s_TiZGaR-WHfAV5KpKr4BJq0r61UTCsEcCQJuy_OSk',
+        range: 'Sheet1!A1:D82',
     }).then(response => {
         data = response.result.values;
 
-        //Display data on console
-        console.log(data);
+        let visited = []; // array of visited programs to avoid duplicates in map
 
-        for (let i = 0; i < data.length; i++) {
-            //taking in all the first column, with all the shcool names 
-            // firstHash.set (i, values[i][0])
-            firstHash[i] = data[i][0]
+        for (let i = 1; i < data.length; i++) {
+            let programName = data[i][0] + ' ' + data[i][1];
+            let lower = data[i][2];
+            let upper = data[i][3];
+
+            // if this program is not already a key in the map, make it one
+            if (visited.indexOf(programName) == -1) {
+                schoolMap.set(programName, {Lower: [], Upper: []});
+                visited.push(programName); // mark program as visited
+            }
+            
+            // if the value is not undefined (an empty cell), add to courselist
+            if (lower)
+                schoolMap.get(programName).Lower.push(String(lower));
+            if (upper)
+                schoolMap.get(programName).Upper.push(String(upper));
         }
-
+        
     }).catch(error => {
         console.error('Error reading data:', error);
     });
 }
+
+console.log(schoolMap);
 
 //show data - need to change this in the future *
 function showData(rowData) {
@@ -77,14 +90,7 @@ function handleFormSubmit(event) {
     var selectedSchool = selectElement.value; //this is the value form the options tag
     console.log(selectedSchool)
 
-    //Option 1: create a for loop to iterate through each of the rows and find the good one. 
-    for (let i = 0; i < data.length; i++) {
-        //taking in all the first column, with all the shcool names 
-        // firstHash.set (i, values[i][0])
-        if (data[i][0] === selectedSchool) {
-            console.log("you selected this school ", selectedSchool)
-        }
-    }
+    console.log("you selected this school ", selectedSchool);
     //Option2: im thinking on using a set instead ** to do not iterate trough all the schools  
 
     //Find the row that matches the selected school
