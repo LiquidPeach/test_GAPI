554a50247153bb8e0a941767d9e87f53db6ebb77
Simone <gaminggyrrl@gmail.com>
2023-05-29 21:10:22 -0700

    Can now display courses on page

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function initClient() {
     // Initialize the Google Sheets API client
     gapi.client.init({
         //i create this apikey on Google console
-        apiKey: '',
-        clientId: '',
-        discoveryDocs: [''],
+        apiKey: 'AIzaSyCss-4b1i0PHAQAjRwKvSN9yPADeaIQS_U',
+        clientId: '964029012495-squ1jhpunge1gvnart6dqintq6qbo1ev.apps.googleusercontent.com',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(() => {
         readData();
     }).catch(error => {
@@ -32,16 +32,22 @@ function readData() {
     gapi.client.sheets.spreadsheets.values.get({
         //this id is unique for every sheet (is on the link)
         //NOTE:Google sheet has to be public *** you can also change the range 
-        spreadsheetId: '',
+        spreadsheetId: '1_s_TiZGaR-WHfAV5KpKr4BJq0r61UTCsEcCQJuy_OSk',
         range: 'Sheet1!A1:D82',
     }).then(response => {
         data = response.result.values;
 
         let visited = []; // array of visited programs to avoid duplicates in map
 
+        // loop through rows and add data to map starting at row 1 (row 0 is the column titles, so ignore)
         for (let i = 1; i < data.length; i++) {
+            // school + program (e.g. UC Irvine + CS B.S.)
             let programName = data[i][0] + ' ' + data[i][1];
+
+            // lower division courses
             let lower = data[i][2];
+            
+            // upper division courses
             let upper = data[i][3];
 
             // if this program is not already a key in the map, make it one
@@ -50,7 +56,7 @@ function readData() {
                 visited.push(programName); // mark program as visited
             }
             
-            // if the value is not undefined (an empty cell), add to courselist
+            // if the value is NOT undefined (an empty cell), add to courselist
             if (lower)
                 schoolMap.get(programName).Lower.push(String(lower));
             if (upper)
@@ -64,41 +70,109 @@ function readData() {
 
 console.log(schoolMap);
 
-//show data - need to change this in the future *
-function showData(rowData) {
-    var tableBody = document.querySelector('#data-table tbody');
-    // Clear existing table body
-    tableBody.innerHTML = '';
+function parseMapData(schoolData, listTag, division) {
 
-    rowData.forEach(cell => {
-        var tableRow = document.createElement('tr');
+    let divisionList = document.getElementById(listTag);
+    divisionList.innerHTML = '';
 
-        cell.forEach(value => {
-            var tableCell = document.createElement('td');
-            tableCell.textContent = value;
-            tableRow.appendChild(tableCell);
-        });
+    for (let course of schoolData[division]) {
+        let list = document.createElement('li');
+        
+        // Highlight OR and AND, and replace special characters
+        let replaceOR = course.replace(/OR/g, '<span class="or">OR</span>');
+        let replaceAND = replaceOR.replace(/AND/g, '<span class="and">AND</span>');
+        let replaceNL = replaceAND.replace(/\n/g, '<br>');
+        
+        let text = replaceNL;
+        
+        let subtitleHTML = '';
+        // '^^' indicates a subtitle, which should have some unique styling
+        let subtitleInx = replaceNL.indexOf('^^');
+        // If '^^' is found, wrap the desired subtitle in a <span> element with a class. Class name is tentative
+        if (subtitleInx !== -1) {
+            let subttl = replaceNL.substring(2, replaceNL.indexOf('<br>'));
+            subtitleHTML = '<span class="sub-field">' + subttl + '</span><br>';
+            text = replaceNL.substring(replaceNL.indexOf('<br>') + 4, replaceNL.length);
+        }
+        
+        // '--' indicates a sublist (a list within the main list). 
+        // startSubLists contains the starting indices of every sublist in the cell
+        let startSubLists = [...text.matchAll(new RegExp('--', 'gi'))].map(a => a.index);
 
-        tableBody.appendChild(tableRow);
-    });
+        if (startSubLists.length > 0) {
+            for (let i = 0; i < startSubLists.length; i++) {
+                let sublist = document.createElement('ul');
+                sublist.classList.add('sublist');
+                
+                // sublistData contains the content from the current sublist to the start of the next
+                //                                                + 2 to disclude the '--' from the string
+                let sublistData = text.substring(startSubLists[i] + 2, startSubLists[i + 1]);
+                
+                // To change the style of the 'title' of the sublist, get the first line break <br> in the
+                // sublist data, and wrap the 'title' in a <span>
+                let br = sublistData.indexOf('<br>') + 4;
+                let ttl = '<span class="bold">' + sublistData.substring(0, br) + '</span>';
+
+                // Append to the HTML of the list. If there was a '^^' found earlier, append the subtitle as well as
+                // the title of the sublist. Set subtitleHTML to '' so not to append the subtitle more than once
+                list.innerHTML += subtitleHTML + ttl;
+                subtitleHTML = '';
+                
+                // Content of sublist data without the title. This will be put into <li> tags
+                let content = sublistData.substring(br, sublistData.length);
+                
+                // Find all the line breaks in the cell. Each line of data will have its own <li> element
+                let indices = [...content.matchAll(new RegExp('<br>', 'gi'))].map(a => a.index);
+            
+                let prev = 0;
+                indices.forEach(function(value) {
+                    let sublistItem = document.createElement('li');
+                    sublistItem.innerHTML = content.substring(prev, value);
+                    
+                    if (sublistItem.innerHTML == '') return;
+
+                    if (sublistItem.innerHTML.indexOf('--') !== -1) {
+                        lastSubListIndex = sublistItem.innerHTML.indexOf('--');
+                        return;
+                    }
+                    sublist.appendChild(sublistItem);
+                    prev = value + 4;
+                });
+                // if loop above does not reach last course
+                let sublistItem = document.createElement('li');
+                sublistItem.innerHTML = content.substring(prev, content.length);
+                if (sublistItem.innerHTML.length > 0)
+                    sublist.appendChild(sublistItem);
+
+                list.appendChild(sublist);
+            }
+        } else {
+            list.innerHTML = subtitleHTML + text;
+        }
+        divisionList.appendChild(list);
+    }
 }
 
 function handleFormSubmit(event) {
     event.preventDefault(); // Prevent form submission
 
-    var selectElement = document.querySelector('#school-select');
-    var selectedSchool = selectElement.value; //this is the value form the options tag
-    console.log(selectedSchool)
+    let selectElement = document.querySelector('#school-select');
+    let selectedSchool = selectElement.value; //this is the value form the options tag
 
-    console.log("you selected this school ", selectedSchool);
-    //Option2: im thinking on using a set instead ** to do not iterate trough all the schools  
+    let schoolData = schoolMap.get(selectedSchool);
 
-    //Find the row that matches the selected school
-    var rowData = data.find(row => row[0] === selectedSchool);
+    // Add selected schools name as the subtitle of the page
+    let subTitle = document.getElementById('school-name-subtitle');
+    subTitle.innerText = selectedSchool;
+    
+    if (schoolData) {
+        let hidden = document.querySelectorAll('details.hidden');
+        for (let tag of hidden) { 
+            tag.classList.remove('hidden');
+        }
+        parseMapData(schoolData, 'lower-division', 'Lower');
+        parseMapData(schoolData, 'upper-division', 'Upper');
 
-    if (rowData) {
-        //display data
-        showData([rowData]);
     } else {
         console.log('No data found for the selected school.');
     }
